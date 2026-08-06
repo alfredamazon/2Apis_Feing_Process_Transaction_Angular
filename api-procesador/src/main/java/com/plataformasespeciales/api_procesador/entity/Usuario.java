abf36bfbab3ba5ef0e2224f716191c60b34a3444
AZamoranoA <alfredo.zamorano@amarello.com.mx>
2026-08-05 20:58:03 -0600

    Ajustes seguridad, login y registro operaciones

--- api-procesador/src/main/java/com/plataformasespeciales/api_procesador/entity/Usuario.java
+++ api-procesador/src/main/java/com/plataformasespeciales/api_procesador/entity/Usuario.java
@@ -1,5 +1,6 @@
 package com.plataformasespeciales.api_procesador.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,12 +10,13 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "usuarios")
+
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name = "usuario", unique = true, nullable = false)
     private String usuario;
 
     private String password;
---
+++ api-procesador/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuarios (usuario, password)
+VALUES ('admin', '$2a$10$WvYW/JKTY6d87f2PEDLFTOj8cEeO.WyF3rKLQTQMPuKyMj9PX0Cle');
